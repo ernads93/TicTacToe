76294ca8467d8acd08c80cf432c8381c8029e20c
ernads <ernadsehic@msn.com>
2020-08-21 17:34:55 +0200

    Maven init

---
+++ src/main/java/org/ernad/App.java
@@ -0,0 +1,13 @@
+package org.ernad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
